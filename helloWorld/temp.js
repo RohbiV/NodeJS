e75c9f31ae8a97af23084749efbaefaf51bafdd0
vasu <vkumarvatsa@gmail.com>
2024-07-16 15:30:00 +0530

    For revision of file handling

--- helloWorld/temp.js
+++ helloWorld/temp.js
@@ -3,3 +3,15 @@
 const os = require("os");
 
 console.log(os.cpus().length);
+
+const fs = require("fs");
+
+fs.writeFileSync("./rev.txt", "This is for revision purpose!");
+
+const res = fs.readFileSync("./rev.txt","utf-8");
+
+console.log(res);
+
+fs.appendFileSync("./rev.txt","this is appeded data");
+
+
