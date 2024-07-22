dbf183d95abf94299a5a6dfa4fb942e1793a5c7b
vasu <vkumarvatsa@gmail.com>
2024-07-22 13:08:59 +0530

    started learning express in nodejs

--- helloWorld/myServer.js
+++ helloWorld/myServer.js
@@ -2,45 +2,56 @@
 //  Creating my server using http module in nodejs
 
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const express = require("express");
 
-const myServer = http.createServer((req,res)=>{
-    console.log("New Request Recieved!");
-    console.log(req.headers);
-    const log = `${Date.now()}: ${req.method} ${req.url} New Request Recieved!!\n`;
-    
-    const myUrl = url.parse(req.url,true);
-    console.log(myUrl);
+const app = express();
 
-
-    fs.appendFile("log.txt", log, (err, data)=> {
-        switch (myUrl.pathname) {
-            case '/': 
-                if(req.method === 'GET') res.end("Home Page");
-                break;
-
-            case '/about': 
-                const userName = myUrl.query.myname;
-                res.end(`Hi ${userName}`);
-                break;
-
-            case '/contact': res.end("Contact Page");
-                break;  
-
-            case '/signup': 
-                if(req.method === 'GET') res.end("This is a signup Form!");
-                else if(req.method === 'POST') {
-                    // db Query
-                    res.end("Success!");
-                }
-
-            default: res.end("404 Page not Found!!"); 
-                break;
-        }
-    });
-
+app.get("/", (req,res) => {
+    return res.send(`Hello from home page! `);
 });
+
+app.get("/about", (req,res) => {
+    return res.send(`Hello from about page ${req.query.name}`);
+}) ;
+
+
+// function myHandler(req,res) {
+//     console.log("New Request Recieved!");
+//     console.log(req.headers);
+//     const log = `${Date.now()}: ${req.method} ${req.url} New Request Recieved!!\n`;
+    
+//     const myUrl = url.parse(req.url,true);
+//     console.log(myUrl);
+
+
+//     fs.appendFile("log.txt", log, (err, data)=> {
+//         switch (myUrl.pathname) {
+//             case '/': 
+//                 if(req.method === 'GET') res.end("Home Page");
+//                 break;
+
+//             case '/about': 
+//                 const userName = myUrl.query.myname;
+//                 res.end(`Hi ${userName}`);
+//                 break;
+
+//             case '/contact': res.end("Contact Page");
+//                 break;  
+
+//             case '/signup': 
+//                 if(req.method === 'GET') res.end("This is a signup Form!");
+//                 else if(req.method === 'POST') {
+//                     // db Query
+//                     res.end("Success!");
+//                 }
+
+//             default: res.end("404 Page not Found!!"); 
+//                 break;
+//         }
+//     })
+// }
+
+const myServer = http.createServer(app);
 
 myServer.listen(2000, () => console.log("Server Started!!"));
 
